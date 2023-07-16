6d3681758c819aa002727750f0400e8545326a91
hritikfront22dhiman <109993618+hritikfront22dhiman@users.noreply.github.com>
2023-07-16 16:44:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function BMI() {
     let bmi = Math.round(Weigth / Math.pow(height, 2));
     document.getElementById('bmi').innerHTML = `Your BMI is ${bmi}`;
 }
+// Get the form element by its id
+let form = document.getElementById("myform");
+
+// Define a function to handle the form submission
+function handleForm(event) {
+  // Prevent the default action of refreshing the page
+  event.preventDefault();
+  // Do something with the form data here
+  // For example, calculate the BMI and display it on the page
+}
+
+// Add an event listener to the form element
+form.addEventListener("Get Your BMI", handleForm);
